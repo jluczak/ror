3dbd5754882e3f5611a5c44d5a832ea39f3300e6
jluczak <jluczaksas@gmail.com>
2019-01-12 13:14:36 +0100

    small changes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,47 +1,47 @@
 class QuotesController < ApplicationController
-    
+
     http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
-    
+
     def index
         @quotes = Quote.all
     end
-    
+
     def show
         @quote = Quote.find(params[:id])
     end
-    
+
     def new
         @quote = Quote.new
     end
-    
+
     def edit
         @quote = Quote.find(params[:id])
     end
 
     def create
         @quote = Quote.new(quote_params)
-    
+
         if @quote.save
             redirect_to @quote
             else
             render 'new'
         end
     end
-    
+
     def update
         @quote = Quote.find(params[:id])
-        
+
         if @quote.update(quote_params)
             redirect_to @quote
         else
             render 'edit'
         end
     end
-    
+
     def destroy
         @quote = Quote.find(params[:id])
         @quote.destroy
-        
+
         redirect_to quotes_path
     end
 
