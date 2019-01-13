eaa1f0b078d6d52cf7bfd2fa94191c40e9464f7b
jluczak <jluczaksas@gmail.com>
2019-01-13 21:06:40 +0100

    small changes in show view, delete authorization

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,5 @@
 class QuotesController < ApplicationController
 
-    http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
-
     def index
         @quotes = Quote.all
     end
